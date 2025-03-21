2e1f1af0793f7fa931f68fcede218702c24f5f59
Nathaniel Ankomah Aidoo <nathanielankomahaidoo@gmail.com>
2025-03-21 15:49:34 +0000

    Resolved merge conflicts in onboardingPage

--- backend/database.py
+++ backend/database.py
@@ -1,22 +1,7 @@
 from sqlalchemy import create_engine, Column, Integer, String, DateTime
-<<<<<<< HEAD
-from sqlalchemy.orm import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import declarative_base, sessionmaker
 from config import DATABASE_URL
-
-=======
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
-
-DATABASE_URL = "postgresql://user:password@localhost/db_name"
->>>>>>> d98c52a478820b0c4d43107ab99ae23ae7ee364d
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
-<<<<<<< HEAD
-
-
-# print(f"Database URL: {DATABASE_URL}")
-=======
->>>>>>> d98c52a478820b0c4d43107ab99ae23ae7ee364d
